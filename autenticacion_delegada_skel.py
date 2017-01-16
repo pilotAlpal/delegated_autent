9c285ef4a83a7afd76c08776d2ec9c96f5b42fdc
adrianmjimuni <adrima05@ucm.es>
2017-01-16 13:04:20 +0100

    Atenticacion realizada, falta anti-csrf

--- autenticacion_delegada_skel.py
+++ autenticacion_delegada_skel.py
@@ -5,15 +5,18 @@
 #
 
 
-from bottle import run, get
+from bottle import run, get, request
 # Resto de importaciones
 
-
+import urllib2, urllib
+import hashlib
+import os
+import json
 # Credenciales. 
 # https://developers.google.com/identity/protocols/OpenIDConnect#appsetup
 # Copiar los valores adecuados.
-CLIENT_ID     = XXXXX
-CLIENT_SECRET = YYYYY
+CLIENT_ID     = "64556356867-83l64d75qv4egbh0lh5go1nslatldu3p.apps.googleusercontent.com"
+CLIENT_SECRET = "jZ6AMAO7TltKpSUZmXDWxgd6"
 REDIRECT_URI  = "http://localhost:8080/token"
 
 
@@ -28,17 +31,24 @@ DISCOVERY_DOC = "https://accounts.google.com/.well-known/openid-configuration"
 TOKEN_VALIDATION_ENDPOINT = "https://www.googleapis.com/oauth2/v4/token"
 
 
-
+    
 
 @get('/login_google')
 def login_google():
-    pass
-
+    state = hashlib.sha256(os.urandom(1024)).hexdigest()
+    p = "https://accounts.google.com/o/oauth2/v2/auth?client_id="+CLIENT_ID+"&response_type=code&scope=openid%20email&redirect_uri="+REDIRECT_URI
+    return urllib2.urlopen(p)
 
 @get('/token')
 def token():
-    pass
-
+    code = request.params.get('code')
+    params = {"code":code,"client_id":CLIENT_ID,"client_secret":CLIENT_SECRET,"grant_type":"authorization_code","redirect_uri":REDIRECT_URI} 
+    params = urllib.urlencode(params)
+    m = urllib2.urlopen(TOKEN_VALIDATION_ENDPOINT, params)
+    m = json.loads(m.read())
+    p = urllib2.urlopen("https://www.googleapis.com/oauth2/v3/tokeninfo?id_token="+m['id_token'])
+    p = json.loads(p.read())
+    return "<b>Bienvenido "+p['email']+"</b>"
 
 if __name__ == "__main__":
     # NO MODIFICAR LOS PARÁMETROS DE run()
