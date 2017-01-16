fa7b2852ddb850f44e7168800afd7fd8f794bd22
adrianmjimuni <adrima05@ucm.es>
2017-01-16 19:59:41 +0100

    100% with anti-csrf

--- autenticacion_delegada_skel.py
+++ autenticacion_delegada_skel.py
@@ -5,7 +5,7 @@
 #
 
 
-from bottle import run, get, request
+from bottle import run, get, request, response
 # Resto de importaciones
 
 import urllib2, urllib
@@ -30,25 +30,29 @@ DISCOVERY_DOC = "https://accounts.google.com/.well-known/openid-configuration"
 # https://developers.google.com/identity/protocols/OpenIDConnect#validatinganidtoken
 TOKEN_VALIDATION_ENDPOINT = "https://www.googleapis.com/oauth2/v4/token"
 
-
+STATE =""
     
 
 @get('/login_google')
 def login_google():
     state = hashlib.sha256(os.urandom(1024)).hexdigest()
-    p = "https://accounts.google.com/o/oauth2/v2/auth?client_id="+CLIENT_ID+"&response_type=code&scope=openid%20email&redirect_uri="+REDIRECT_URI
+    response.set_cookie('state', state)
+    p = "https://accounts.google.com/o/oauth2/v2/auth?client_id="+CLIENT_ID+"&response_type=code&scope=openid%20email&redirect_uri="+REDIRECT_URI+"&state="+state
     return urllib2.urlopen(p)
 
 @get('/token')
 def token():
-    code = request.params.get('code')
-    params = {"code":code,"client_id":CLIENT_ID,"client_secret":CLIENT_SECRET,"grant_type":"authorization_code","redirect_uri":REDIRECT_URI} 
-    params = urllib.urlencode(params)
-    m = urllib2.urlopen(TOKEN_VALIDATION_ENDPOINT, params)
-    m = json.loads(m.read())
-    p = urllib2.urlopen("https://www.googleapis.com/oauth2/v3/tokeninfo?id_token="+m['id_token'])
-    p = json.loads(p.read())
-    return "<b>Bienvenido "+p['email']+"</b>"
+    if request.cookies.get('state') == request.params.get('state'):
+        code = request.params.get('code')
+        params = {"code":code,"client_id":CLIENT_ID,"client_secret":CLIENT_SECRET,"grant_type":"authorization_code","redirect_uri":REDIRECT_URI} 
+        params = urllib.urlencode(params)
+        m = urllib2.urlopen(TOKEN_VALIDATION_ENDPOINT, params)
+        m = json.loads(m.read())
+        p = urllib2.urlopen("https://www.googleapis.com/oauth2/v3/tokeninfo?id_token="+m['id_token'])
+        p = json.loads(p.read())
+        return "<b>Bienvenido "+p['email']+"</b>"
+    else:
+        return "<b>ERROR</b>"
 
 if __name__ == "__main__":
     # NO MODIFICAR LOS PARÁMETROS DE run()
